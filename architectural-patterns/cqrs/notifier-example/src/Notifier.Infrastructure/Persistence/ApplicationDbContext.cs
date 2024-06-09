aa7316afa364109f0bddd0a3b48108ba625cf61b
juanlavallen <juanlavallen02@gmail.com>
2024-06-09 20:24:35 -0300

    feat: application context created

---
+++ architectural-patterns/cqrs/notifier-example/src/Notifier.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Notifier.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+    }
+}
