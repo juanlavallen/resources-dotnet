8bebdbefb00b3c691263287731f97ca722080287
juanlavallen <juanlavallen02@gmail.com>
2024-06-09 20:26:15 -0300

    feat: database connection added

--- architectural-patterns/cqrs/notifier-example/src/Notifier.Infrastructure/DependencyInjection.cs
+++ architectural-patterns/cqrs/notifier-example/src/Notifier.Infrastructure/DependencyInjection.cs
@@ -1,3 +1,19 @@
-﻿namespace Notifier.Infrastructure;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notifier.Infrastructure.Persistence;
 
-public static class DependencyInjection { }
+namespace Notifier.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("Default"));
+        });
+
+        return services;
+    }
+}
